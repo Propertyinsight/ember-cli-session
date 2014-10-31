bee56ac6508806fcd70a1d78417a68d3cbbf89b4
Johnny Oshika <johnnyoshika@gmail.com>
2014-10-30 23:14:21 -0700

    Added sign out

---
+++ addon/routes/authentication/signout.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+import ajax from 'ic-ajax';
+import config from '../../config/environment';
+
+export default Ember.Route.extend({
+
+    beforeModel: function(transition) {
+        return ajax({
+            type: 'PUT',
+            url: '/api_v1/authentication/signout'
+        }).then(function(response) {
+            window.location.href = config.baseURL + 'authentication';
+        }, function(reason) {
+            this.replaceWith('authentication/signout-failed');
+        });
+    }
+
+});
