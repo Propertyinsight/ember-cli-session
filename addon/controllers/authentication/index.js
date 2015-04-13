b7a85d55af24755870d5d9124093be7e0ca5ed09
Johnny Oshika <johnnyoshika@gmail.com>
2015-04-13 08:15:21 -0700

    authentication/index no longer extends deprecated ObjectController

--- addon/controllers/authentication/index.js
+++ addon/controllers/authentication/index.js
@@ -1,11 +1,11 @@
 import Ember from 'ember';
 import ajax from 'ic-ajax';
 
-export default Ember.ObjectController.extend({
+export default Ember.Controller.extend({
 
     actions: {
         signIn: function() {
-            
+
             this.set('signingIn', true);
             ajax({
                 type: 'POST',
@@ -13,16 +13,16 @@ export default Ember.ObjectController.extend({
                 contentType: 'application/json',
                 data: JSON.stringify({
                     user: {
-                        email: this.get('email'),
-                        password: this.get('password'),
-                        remember_me: this.get('rememberMe')
+                        email: this.get('model.email'),
+                        password: this.get('model.password'),
+                        remember_me: this.get('model.rememberMe')
                     }
                 })
             }).then(function() {
                 this.set('signingIn', false);
 
                 if (!this.session.tryTransitionOnSignIn())
-                    this.transitionToRoute('portal');				
+                    this.transitionToRoute('portal');
             }.bind(this), function(response) {
 
                 this.set('signingIn', false);
