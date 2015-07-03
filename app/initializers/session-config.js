c387113f4125ecae218fa5766b645386141c86bd
Brandon J McKay <admin@brandonjmckay.com>
2015-07-03 16:28:01 -0700

    pretty code

--- app/initializers/session-config.js
+++ app/initializers/session-config.js
@@ -1,15 +1,17 @@
 export function initialize(container, application) {
 
-    var config = {
-        baseURL: application.get('baseURL'),
-        apiNamespace: application.get('apiNamespace')
-    };
+  var config = {
+    baseURL: application.get('baseURL'),
+    apiNamespace: application.get('apiNamespace')
+  };
 
-    application.register('config:session-config', config, {instantiate: false});
+  application.register('config:session-config', config, {
+    instantiate: false
+  });
 
-    ['route', 'controller', 'view'].forEach(function(componentType) {
-        application.inject(componentType, 'session-config', 'config:session-config');
-    });
+  ['route', 'controller', 'view'].forEach(function(componentType) {
+    application.inject(componentType, 'session-config', 'config:session-config');
+  });
 
 };
 
